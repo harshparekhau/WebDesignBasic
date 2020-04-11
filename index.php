4cf824ee7debaa07b126d985db8aba1787b69f78
harshparekhau <40466470+harshparekhau@users.noreply.github.com>
2020-04-11 16:38:35 +1000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>JOB Application</title>
+	<?php include 'inc\head.inc';; ?>
+	<!-- References to CSS file' -->
+	<link rel="stylesheet" type="text/css" href="styles/style.css">
+</head>
+<body class="home">
+	<?php
+	
+
+	include 'inc\menu.inc';
+	?>
+	
+	<p class="space"><br/></p>
+		<h1 class="Welcome">WELCOME TO OUR JOB SEARCH</h1>
+	<?php
+	include 'footer.inc';
+	?>
+</body>
+</html>
